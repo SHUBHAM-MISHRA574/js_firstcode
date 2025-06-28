7552634ccb9e7db7d0cfcb73c266912a3b99a283
SHUBHAM-MISHRA574 <shubhammishrabxr13@gmail.com>
2025-06-28 12:58:35 +0000

    commit the changes

---
+++ 01_basics/04_memory.js
@@ -0,0 +1,13 @@
+const gamename="Shubham"
+console.log(gamename)
+console.log(gamename[0])
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('b'))
+const newstring=gamename.substring(0,4)
+console.log(newstring)
+const newstring1="    shubham    "
+console.log(newstring1.trim());
+
+
